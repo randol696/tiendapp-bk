8d63f4360b3f9344d5e03996cd2498f510f3907d
Randol Gonzalez <randol696@live.com>
2024-05-13 16:54:29 -0500

    http added domain from

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const pool = new Pool({
 });
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://tiendapp-bk.onrender.com'); // update to match the domain you will make the request from
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173'); // update to match the domain you will make the request from
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   next();
